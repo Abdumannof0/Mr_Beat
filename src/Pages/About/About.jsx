3473030fdf56da1d7095420cbb693b8887a382cf
Abduvaliev <140178160+Abdumannof0@users.noreply.github.com>
2023-12-27 18:45:53 +0300

    #fix #Redux #Navigation #json-server json-server yaratildi va redux bilan ishlash boshlandi

---
+++ src/Pages/About/About.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import AboutWe from '../../components/About/AboutWe'
+
+export default function About() {
+    return (
+        <>
+            <AboutWe />
+        </>
+    )
+}
